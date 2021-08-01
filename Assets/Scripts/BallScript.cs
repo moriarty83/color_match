8c7153481ba6b97dc38b0fb69c277f70e946f1e6
moriarty83 <moriarty83@gmail.com>
2021-08-01 19:43:07 -0400

    First commit. Basic functionalit: display camera image on screen, generate random color to match, compare that color with pixel the player clicks

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private Material material;
+    private Color32 colorToMatch;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = this.gameObject.GetComponent<Renderer>().material;
+        NewMatchColor();
+        
+    }
+
+    // Creates a random color and applies it to the Sphere
+    public void NewMatchColor()
+    {
+        colorToMatch = Random.ColorHSV();
+        material.color = colorToMatch;
+        // Debug.Log(color.r);
+        
+    }
+
+    // TODO Come up with a better scoring method.
+    // Returns a single number represneting the absolute values of the RGB values
+    // subtracted from 255. The closer the clicked and matched colors are, the higher value is returned.
+    // Kept in the sphere script so the Sphere color can remain private.
+    public int compareColors(Color32 clickedColor)
+    {
+        int rAbsolute = Mathf.Abs(255 - (Mathf.Abs(colorToMatch.r - clickedColor.r)));
+        int gAbsolute = Mathf.Abs(255 - (Mathf.Abs(colorToMatch.g - clickedColor.g)));
+        int bAbsolute = Mathf.Abs(255 - (Mathf.Abs(colorToMatch.b - clickedColor.b)));
+
+        return rAbsolute + gAbsolute + bAbsolute;
+    }
+}
---
+++ Assets/Scripts/CameraImage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraImage : MonoBehaviour
+{
+// Camera display variables
+    // rawimage is what the camera image is displayed on.
+    public RawImage rawimage;
+    // WebCamTextures are what get the image from the device camera.
+    private WebCamTexture webCamTexture;
+
+    // Sphere script
+
+    private BallScript ballScript;
+
+    void Start()
+    {
+        ballScript = GameObject.Find("Sphere").GetComponent<BallScript>();
+        webCamTexture = new WebCamTexture();
+        rawimage.texture = webCamTexture;
+        rawimage.material.mainTexture = webCamTexture;
+        rawimage.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+        webCamTexture.Play();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Vector3 mousePosition = Input.mousePosition;
+            
+            Debug.Log(Input.mousePosition);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 100f))
+            {
+                if(hit.transform.name == "Sphere")
+                {
+                    ballScript.NewMatchColor();
+                }
+                Debug.Log("You selected the " + hit.transform.name);
+                return;
+            }
+            else
+            {
+                Color32 clickedColor = webCamTexture.GetPixel((int)mousePosition.x, (int)mousePosition.y);
+                Debug.Log(ballScript.compareColors(clickedColor));
+
+            }
+        }
+    }
+
+}
+
